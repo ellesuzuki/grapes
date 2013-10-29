8f10872848359d2fa80cd9580e6d987228ddc8bc
Elle Suzuki <elle.suzuki@gmail.com>
2013-10-29 00:53:34 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grapes::Application.config.secret_token = 'd06395474a3f248c444803ccf0f6f16d7184be84e683bbaf927ab4082ca28c6e8953e0e4ce5dac6acccb7e9d094664cf674db5ff17a0f39f771b29eb53758577'
